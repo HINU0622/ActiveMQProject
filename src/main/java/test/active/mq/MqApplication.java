f63d5e08724ae62895bb21594921900cd643bece
HINU0622 <HINU0622@gmail.com>
2023-09-14 19:39:13 +0900

    불 필요한 ComponentScan 어노테이션 제거

--- src/main/java/test/active/mq/MqApplication.java
+++ src/main/java/test/active/mq/MqApplication.java
@@ -2,8 +2,11 @@ package test.active.mq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class MqApplication {
 
 	public static void main(String[] args) {
