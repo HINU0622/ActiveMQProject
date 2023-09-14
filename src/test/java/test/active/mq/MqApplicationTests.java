f70411d859a3abf3dd70f4707331d99db13b7451
HINU0622 <HINU0622@gmail.com>
2023-09-14 18:58:58 +0900

    Initial commit

---
+++ src/test/java/test/active/mq/MqApplicationTests.java
@@ -0,0 +1,13 @@
+package test.active.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
